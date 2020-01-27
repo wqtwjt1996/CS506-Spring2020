cc75ea630d4b07513ca4273e7571c73cbaa243ae
wqtwjt1996 <1061687050@qq.com>
2020-01-27 17:20:03 -0500

    Qitong Wang changed something...
    Qitong Wang changed lake...

--- 01-python/outdoors/lake.py
+++ 01-python/outdoors/lake.py
@@ -1,3 +1,11 @@
 def draw_lake():
-    print("lake not found")
+    # print("lake not found")
+    print("water")
+    print("water")
+    print("water")
+    print("water")
+    print("water")
+    print("water")
+    print("water")
+    print("water")
     return
